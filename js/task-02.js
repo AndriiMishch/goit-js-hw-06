1cde583356c1df89097a2e5f8a647e74c0a2081e
AndriiMishch <anderito02@gmail.com>
2023-06-18 19:12:34 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients')
+
+for(let ingredient of ingredients) {
+  const element = document.createElement("li");
+  element.textContent = ingredient;
+  element.classList = 'item';
+  list.append(element);
+}
