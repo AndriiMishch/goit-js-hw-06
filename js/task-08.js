e992a98a7ee02c956326393b5fca8d77bd4f0ecd
AndriiMishch <anderito02@gmail.com>
2023-06-18 19:12:45 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", submit);
+
+
+function submit(event) {
+    event.preventDefault()
+
+    const formEl = event.currentTarget.elements;
+    const mail = formEl.email.value;
+    const password = formEl.password.value;
+
+    if(mail === "" || password === "") {
+        alert("Всі поля мають бути заповненими")
+    } else {
+        const formData = {
+            mail,
+            password,
+        };
+        console.log(formData)
+
+        form.reset()
+    }
+}
